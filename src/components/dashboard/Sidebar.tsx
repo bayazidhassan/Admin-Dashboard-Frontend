26c132579febed3333f39a0e25d25ee3d9f6c4bf
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-31 10:41:07 +0600

    Sidebar

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -1,8 +1,22 @@
+import { NavLink } from 'react-router-dom';
+
 const Sidebar = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    <aside className="w-64 bg-gray-900 text-white">
+      <div className="p-5 text-xl font-bold">Admin Dashboard</div>
+
+      <nav className="flex flex-col">
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        <NavLink to="/dashboard/permissions">Permissions</NavLink>
+        <NavLink to="/dashboard/roles">Roles</NavLink>
+        <NavLink to="/dashboard/users">Users</NavLink>
+        <NavLink to="/dashboard/media">Media</NavLink>
+        <NavLink to="/dashboard/categories">Categories</NavLink>
+        <NavLink to="/dashboard/brands">Brands</NavLink>
+        <NavLink to="/dashboard/attributes">Attributes</NavLink>
+        <NavLink to="/dashboard/products">Products</NavLink>
+      </nav>
+    </aside>
   );
 };
 
