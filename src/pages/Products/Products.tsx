4a166f250677f6f5731921f313b8edf8ac804565
bayazidhassan <bayazidhassan776@gmail.com>
2026-08-01 05:51:48 +0600

    update product

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -6,6 +6,8 @@ import {
   useCreateProductMutation,
   useDeleteProductMutation,
   useGetProductsQuery,
+  useUpdateProductMutation,
+  type Product,
 } from '../../features/product/productApi';
 
 const Product = () => {
@@ -28,11 +30,14 @@ const Product = () => {
   const [categoryIds, setCategoryIds] = useState<string[]>([]);
   const [deleteProduct, { isLoading: isDeleting }] = useDeleteProductMutation();
   const [deletingId, setDeletingId] = useState<string | null>(null);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const [isEdit, setIsEdit] = useState(false);
 
   const { data: brandsData } = useGetBrandsQuery();
   const { data: categoriesData } = useGetCategoriesQuery();
 
   const [createProduct, { isLoading: isCreating }] = useCreateProductMutation();
+  const [updateProduct, { isLoading: isUpdating }] = useUpdateProductMutation();
 
   const handleCreateProduct = async () => {
     try {
@@ -74,6 +79,65 @@ const Product = () => {
     }
   };
 
+  const handleEditProduct = (product: Product) => {
+    setSelectedProduct(product);
+
+    setName(product.name);
+    setSlug(product.slug);
+    setSku(product.sku);
+
+    setShortDescription(product.shortDescription ?? '');
+    setLongDescription(product.longDescription ?? '');
+
+    setPrice(product.price);
+    setSalePrice(product.salePrice ?? 0);
+    setStock(product.stock);
+    setWeight(product.weight ?? 0);
+
+    setActive(product.active);
+    setFeatured(product.featured);
+    setSortOrder(product.sortOrder);
+
+    setBrandId(product.brandId ?? '');
+
+    setCategoryIds(product.categories.map((category) => category.id));
+
+    setIsEdit(true);
+    setOpenModal(true);
+  };
+
+  const handleUpdateProduct = async () => {
+    if (!selectedProduct) return;
+
+    try {
+      await updateProduct({
+        id: selectedProduct.id,
+        body: {
+          name,
+          slug,
+          sku,
+          shortDescription,
+          longDescription,
+          price,
+          salePrice,
+          stock,
+          weight,
+          active,
+          featured,
+          sortOrder,
+          brandId: brandId || undefined,
+          categoryIds,
+        },
+      }).unwrap();
+
+      setOpenModal(false);
+      setSelectedProduct(null);
+      setIsEdit(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleDeleteProduct = async (id: string) => {
     const confirmDelete = window.confirm(
       'Are you sure you want to delete this product?',
@@ -101,7 +165,30 @@ const Product = () => {
         <h1 className="text-2xl font-bold">Products</h1>
 
         <button
-          onClick={() => setOpenModal(true)}
+          onClick={() => {
+            setIsEdit(false);
+            setSelectedProduct(null);
+
+            setName('');
+            setSlug('');
+            setSku('');
+            setShortDescription('');
+            setLongDescription('');
+
+            setPrice(0);
+            setSalePrice(0);
+            setStock(0);
+            setWeight(0);
+
+            setActive(true);
+            setFeatured(false);
+            setSortOrder(0);
+
+            setBrandId('');
+            setCategoryIds([]);
+
+            setOpenModal(true);
+          }}
           className="cursor-pointer rounded bg-black px-4 py-2 text-white"
         >
           Create Product
@@ -140,7 +227,10 @@ const Product = () => {
                 </td>
 
                 <td className="space-x-2 px-4 py-3">
-                  <button className="cursor-pointer rounded bg-blue-500 px-3 py-1 text-white">
+                  <button
+                    onClick={() => handleEditProduct(product)}
+                    className="cursor-pointer rounded bg-blue-500 px-3 py-1 text-white"
+                  >
                     Edit
                   </button>
 
@@ -192,10 +282,10 @@ const Product = () => {
         setCategoryIds={setCategoryIds}
         brands={brandsData?.data.items ?? []}
         categories={categoriesData?.data.items ?? []}
-        isLoading={isCreating}
+        isLoading={isCreating || isUpdating}
         isEdit={false}
         onClose={() => setOpenModal(false)}
-        onSave={handleCreateProduct}
+        onSave={isEdit ? handleUpdateProduct : handleCreateProduct}
       />
     </div>
   );
