b917120ce3c5876f78167b5221c96c6e53c091c8
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-31 22:49:02 +0600

    add role

--- src/pages/Roles/Roles.tsx
+++ src/pages/Roles/Roles.tsx
@@ -1,15 +1,70 @@
-import { useGetRolesQuery } from '../../features/role/roleApi';
+import { useState } from 'react';
+import RoleModal from '../../components/RoleModal';
+import { useGetPermissionGroupsQuery } from '../../features/permission/permissionApi';
+import {
+  useCreateRoleMutation,
+  useGetRolesQuery,
+} from '../../features/role/roleApi';
 
 const Roles = () => {
+  const [openModal, setOpenModal] = useState(false);
+
+  const [name, setName] = useState('');
+
+  const [description, setDescription] = useState('');
+
+  const [status, setStatus] = useState(true);
+
+  const [permissionIds, setPermissionIds] = useState<string[]>([]);
   const { data, isLoading, error } = useGetRolesQuery();
+
+  const { data: permissionData } = useGetPermissionGroupsQuery();
+
+  const [createRole, { isLoading: isCreating }] = useCreateRoleMutation();
 
   if (isLoading) return <h2>Loading...</h2>;
 
   if (error) return <h2>Something went wrong.</h2>;
 
+  const handleCreate = async () => {
+    if (!name.trim()) return;
+
+    if (permissionIds.length === 0) {
+      alert('Select at least one permission');
+      return;
+    }
+
+    try {
+      await createRole({
+        name,
+        description,
+        status,
+        permissionIds,
+      }).unwrap();
+
+      setOpenModal(false);
+
+      setName('');
+      setDescription('');
+      setStatus(true);
+      setPermissionIds([]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1 className="mb-6 text-3xl font-bold">Roles</h1>
+
+      <div className="mb-6">
+        <button
+          onClick={() => setOpenModal(true)}
+          className="rounded bg-blue-600 px-4 py-2 text-white"
+        >
+          Create Role
+        </button>
+      </div>
 
       <table className="w-full border">
         <thead>
@@ -41,6 +96,22 @@ const Roles = () => {
           ))}
         </tbody>
       </table>
+
+      <RoleModal
+        open={openModal}
+        name={name}
+        setName={setName}
+        description={description}
+        setDescription={setDescription}
+        status={status}
+        setStatus={setStatus}
+        permissionIds={permissionIds}
+        setPermissionIds={setPermissionIds}
+        permissionGroups={permissionData?.data.items ?? []}
+        isLoading={isCreating}
+        onClose={() => setOpenModal(false)}
+        onSave={handleCreate}
+      />
     </div>
   );
 };
