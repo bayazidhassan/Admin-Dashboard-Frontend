e5c14f48f04016a727800da1d8494b88f43f1f0f
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-31 09:49:10 +0600

    ProtectedRoute

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,15 @@
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import type { RootState } from '../app/storeTypes';
+
+const ProtectedRoute = () => {
+  const { accessToken } = useSelector((state: RootState) => state.auth);
+
+  if (!accessToken) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
